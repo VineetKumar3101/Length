86ae258b46caddb9386da50927c39d5924aa7ae7
Vineet Kumar <vineet.kumar_cs20@gla.ac.in>
2022-01-05 13:15:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vineet/glaapp2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.vineet.glaapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+EditText e1;
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.editText);
+        b1=(Button)findViewById(R.id.button);
+        e1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1=e1.getText().toString();
+                Toast.makeText(MainActivity.this, s1, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
